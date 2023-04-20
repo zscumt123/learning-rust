36e0e3c68d25d06132da94332d28814947d08aff
zscumt123 <zscumt123@163.com>
2023-04-20 19:32:47 +0800

    f

--- src/rgrep/src/main.rs
+++ src/rgrep/src/main.rs
@@ -1,4 +1,9 @@
+use anyhow::Result;
 use clap::Parser;
+use colored::Colorize;
+use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -9,9 +14,29 @@ struct Cli {
     /// Search file
     file: String,
 }
-
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
+    let file = File::open(args.file)?;
+    let reader = BufReader::new(file);
+    let reg_str = format!("{}", args.text);
+    let re = Regex::new(&reg_str).unwrap();
+    for (i, line) in reader.lines().enumerate() {
+        let text = line.unwrap();
 
-    println!("{:?}", args);
+        if re.is_match(&text) {
+            let mut s = String::new();
+            let mut prev: usize = 0;
+            for mat in re.find_iter(&text) {
+                s.push_str(&text[prev..mat.start()]);
+                let n = String::from(&text[mat.start()..mat.end()]).red().bold();
+                println!("{}", n);
+                s.push_str(&n);
+                prev = mat.end();
+            }
+            s.push_str(&text[prev..]);
+            println!("{}: {}", i, s);
+        }
+    }
+    println!("{}", "abc".red().bold());
+    Ok(())
 }
